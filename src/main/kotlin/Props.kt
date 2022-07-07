b6c87b64a537a967bb67ec210eec1b875df86ec9
Jan Jongerden <jan.jongerden@gmail.com>
2022-07-07 16:50:05 +0200

    Add "tags" to list of known properties

--- src/main/kotlin/Props.kt
+++ src/main/kotlin/Props.kt
@@ -2,6 +2,7 @@ object Props {
     const val SHOW_IN_BLOG_LIST = "show_in_blog_list"
     const val TITLE = "title"
     const val DATE = "date"
+    const val TAGS = "tags"
 
-    val knownProps = setOf(SHOW_IN_BLOG_LIST, TITLE, DATE)
+    val knownProps = setOf(SHOW_IN_BLOG_LIST, TITLE, DATE, TAGS)
 }
