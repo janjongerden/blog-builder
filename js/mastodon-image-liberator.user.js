9fcefd866b5226ba5cf738544d6e4b1f7ec13d65
Jan Jongerden <jan.jongerden@gmail.com>
2024-05-22 12:11:52 +0200

    Update remove div blog
    Replace outdated nu.nl example with Mastodon image cleaner example.

---
+++ js/mastodon-image-liberator.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Mastodon Image Liberator
+// @namespace    janjongerden.nl
+// @version      1.0
+// @description  Hide the buttons on top of images
+// @author       Jan Jongerden
+// @include      https://mas.to/*
+// @grant        none
+// ==/UserScript==
+
+const cleanButtons = () => {
+    const hideButtons = document.querySelectorAll('.icon-button.overlayed');
+
+    hideButtons.forEach(element => {
+        element.style.display = 'none';
+    });
+
+    const altButtons = document.querySelectorAll('.media-gallery__item__badges');
+
+    altButtons.forEach(element => {
+        element.style.bottom = '-9px';
+    });
+};
+
+var repeater = setInterval(cleanButtons, 1000);
